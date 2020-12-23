153d30934df6d24e82a9d555914ea8c92c6c04f0
ramon <monchouam@gmail.com>
2020-12-23 17:25:18 -0500

    se agrega codigo de una prueba

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log("hola a todos")
+const nextLarger = (a) => {
+    const res = [];
+    for (let i = 0; i < a.length; i++) {
+        let currNum = a[i];
+        for (let n = i; n < a.length; n++) {
+            if (a[n] > currNum) {
+                res.push(a[n]);
+                break;
+            } else if (n === a.length - 1) {
+                res.push(-1);
+            }
+        }
+    }
+    return res
+}
+let arr = [6, 7, 3, 8];
+console.log(nextLarger(arr))
